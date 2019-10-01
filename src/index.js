362e59ce1445e54d4d87df451808642df28640eb
Nikita Zhavoronkov <larkgronick@gmail.com>
2019-10-01 17:56:22 +0300

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -1,44 +1,56 @@
 const MORSE_TABLE = {
-    '.-':     'a',
+    '.-':     'a', 
     '-...':   'b',
     '-.-.':   'c',
     '-..':    'd',
-    '.':      'e',
+    '.':      'e', 
     '..-.':   'f',
     '--.':    'g',
-    '....':   'h',
+    '....':   'h', 
     '..':     'i',
     '.---':   'j',
     '-.-':    'k',
     '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
+    '--':     'm',  
+    '-.':     'n', 
+    '---':    'o', 
+    '.--.':   'p', 
+    '--.-':   'q', 
+    '.-.':    'r',  
+    '...':    's', 
+    '-':      't', 
+    '..-':    'u',  
+    '...-':   'v', 
+    '.--':    'w', 
+    '-..-':   'x', 
+    '-.--':   'y', 
+    '--..':   'z', 
+    '.----':  '1', 
+    '..---':  '2', 
+    '...--':  '3', 
+    '....-':  '4', 
+    '.....':  '5', 
+    '-....':  '6', 
+    '--...':  '7', 
+    '---..':  '8', 
+    '----.':  '9', 
+    '-----':  '0', 
+}; 
 
 function decode(expr) {
-    // write your solution here
+ 
+    var separateArray = expr.match(/.{1,10}/g);
+    var separateString = separateArray.join(' ');
+    var dashDecoder = separateString.replace(/11/g, "-");
+    var dotDecoder = dashDecoder.replace(/10/g, ".");
+    var deleteZeros = dotDecoder.replace(/0/g, "");
+    var convertation = deleteZeros.replace(/([.-]+[-.]*)/g, (_, x) =>MORSE_TABLE [x]);
+    var deleteSpaces = convertation.replace(/\s/g, '');
+    var finalDecoder = deleteSpaces.replace(/\*\*\*\*\*\*\*\*\*\*/g, " ");
+
+    return finalDecoder;
+
+
 }
 
 module.exports = {
